02c0a7cde8266eeaa986a672edb5135771809267
uguroktay <56156686+uguroktay@users.noreply.github.com>
2024-04-15 09:58:18 +0300

    Add files via upload

---
+++ Module3/files/index.php
@@ -0,0 +1,18 @@
+
+
+<html>
+
+<head>
+
+<title>Advanced Ansible Automation!</title>
+
+</head>
+
+<body>
+
+<h1>Welcome!</h1>
+<p> This app is running on: <?=gethostname()?> </p>
+
+</body>
+
+</html>
